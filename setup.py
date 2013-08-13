69f1c5b7f38bf2574714d4c58f66332e77eaf622
Ken Hu <whosbacon@gmail.com>
2013-08-13 17:36:46 -0400

    include setup script for pip use

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import datetimes
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'datetimes',
+]
+
+requires = []
+
+setup(
+    name='datetimes',
+    version=requests.__version__,
+    description='Extended Python Datetime',
+    long_description=open('README.rst').read(),
+    author='Soshio',
+    author_email='tech@getsoshio.com',
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE'], 'datetimes': ['*.pem']},
+    package_dir={'datetimes': 'datetimes'},
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+
+    ),
+)
